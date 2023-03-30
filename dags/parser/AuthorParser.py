ee8be370d2291ab6b2ecda685ff4386436636778
morn1n9st4r <a.lepilo.soft@gmail.com>
2023-03-30 22:41:33 +0300

    error handling of non-existend or empty fields

--- dags/parser/AuthorParser.py
+++ dags/parser/AuthorParser.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import bs4
 from urllib.request import urlopen, Request
 import pandas as pd
 import re
@@ -28,19 +29,29 @@ class AuthorParser:
 
         # scrape some particular areas of page
         authorid = re.sub(r'-.*', '', re.sub(r'https\:\/\/www\.livelib\.ru\/author\/', '', self.url))
-        name = soup.find("span", {"class": "header-profile-login"})
-        origname = soup.find("span", {"class": "header-profile-status"})
-        liked = soup.find("span", {"title": "Писатель понравился"})
-        neutral = soup.find("span", {"title": "Отнеслись нейтрально"})
-        disliked = soup.find("span", {"title": "Писатель не понравился"})
-        favorite = soup.find("span", {"title": "Почитатели творчества"})
-        reading = soup.find("span", {"title": "Читателей"})
-        print(authorid)
-        print(name.text)
-        print(origname.text)
-        print(re.sub('\n+', '', liked.text))
-        print(re.sub('\n+', '', neutral.text))
-        print(re.sub('\n+', '', disliked.text))
-        print(re.sub('\n+', '', favorite.text))
-        print(re.sub('\n+', '', reading.text))
+        name = soup.find("span", {"class": "header-profile-login"}) or ['None']
+        origname = soup.find("span", {"class": "header-profile-status"}) or ['None']
+        liked = soup.find("span", {"title": "Писатель понравился"}) or [0]
+        neutral = soup.find("span", {"title": "Отнеслись нейтрально"}) or [0]
+        disliked = soup.find("span", {"title": "Писатель не понравился"}) or [0]
+        favorite = soup.find("span", {"title": "Почитатели творчества"}) or [0]
+        reading = soup.find("span", {"title": "Читателей"}) or [0]
+
+        return pd.DataFrame(
+            data ={
+                'AuthorID':[authorid],
+                'Name':[self.get_text(name)],
+                'OriginalName':[self.get_text(origname)],
+                'Liked':[self.get_text(liked)],
+                'Neutral':[self.get_text(neutral)],
+                'Disliked':[self.get_text(disliked)],
+                'Favorite':[self.get_text(favorite)],
+                'Reading':[self.get_text(reading)]
+            }
+        )
             
+    def get_text(self, val):
+        if type(val) == bs4.element.Tag:
+            return re.sub('\n+', '', val.text)
+        else: 
+            return str(*val)
